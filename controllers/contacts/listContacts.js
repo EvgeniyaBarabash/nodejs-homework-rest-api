32d33d0bb60306a74221422184a2c0ad209be18a
EvgeniyaBarabash <barabash1989@gmail.com>
2022-02-06 23:40:43 +0200

    filter favorite

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -2,9 +2,10 @@ const { Contact } = require("../../models/contact");
 
 const listContact = async (req, res) => {
     const { _id } = req.user;
-    const { page = 1, limit = 20 } = req.query;
+    const { page = 1, limit = 20, favorite } = req.query;
+    const filterFavorite = favorite ? { owner: _id, favorite } : { owner: _id };
     const skip = (page - 1) * limit;
-    const contacts = await Contact.find({ owner: _id }, "-createdAt -updatedAt", { skip, limit: +limit }).populate("owner", "email");
+    const contacts = await Contact.find(filterFavorite, "-createdAt -updatedAt", { skip, limit: +limit }).populate("owner", "email");
     res.json({
         status: "success",
         code: 200,
